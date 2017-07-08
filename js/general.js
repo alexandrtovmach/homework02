4e7f1f25f292b184bc2efe069c9667915a12b2ce
Alexandr Tovmach <alexandrtovmach@gmail.com>
2017-07-08 15:37:18 +0300

    done
    fixed:
    
    - [desktop] show wallarper size
    - [pocket] js function showSideBar()
    
    add:
    
    - [small] style for small devices
    - [small] js function smallMenuShow()

--- js/general.js
+++ js/general.js
@@ -3,15 +3,32 @@
  */
 function init() {
     document.getElementsByClassName('c_aside')[0].addEventListener('click', showSideBar);
+	document.addEventListener('click', function (event) {
+		if ((!phaseSmallMenu)||(event.target == document.getElementsByClassName('h_panelBar')[0])||
+		(event.target == document.getElementsByTagName('INPUT')[0])||
+		(event.target == document.getElementsByClassName('h_search')[0])) {return}
+		document.getElementsByClassName('h_panel')[0].style.display = 'none';
+		phaseSmallMenu = 0;
+	});
 }
-var phase = 0;
+
+var phaseSideBar = 0;
+var phaseSmallMenu = 0;
+
 function showSideBar() {
     var element = document.getElementsByClassName('c_aside')[0]
-    if (!phase) {
+    if (!phaseSideBar) {
         element.firstElementChild.style.left = '0px';
-        phase = 1;
+        phaseSideBar = 1;
     } else {
-        event.target.parentElement.firstElementChild.style.left = '';
-        phase = 0;
+        element.firstElementChild.style.left = '';
+        phaseSideBar = 0;
     }
+}
+function smallMenuShow() {
+	var elem = document.getElementsByClassName('h_panelBar')[0];
+	elem.previousElementSibling.style.display = 'block';
+	elem.previousElementSibling.style.top = elem.getBoundingClientRect().top + 40 + 5 + 'px';
+	elem.previousElementSibling.style.right = '5px';
+	phaseSmallMenu = 1;
 }
